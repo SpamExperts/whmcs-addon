14215cee24b4570960617a93cb64be1c542a4f26
teo <laurentiu.teodorescu@n-able.com>
2024-05-23 16:45:16 +0300

    Specify compression args

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ -f ./latest.tar.gz ]; then
     rm ./latest.tar.gz
 fi
 
-tar -czf ./latest.tar.gz --owner=0 --group=0 ./modules
+tar -I 'gzip -n' -cf ./latest.tar.gz --owner=0 --group=0 ./modules
